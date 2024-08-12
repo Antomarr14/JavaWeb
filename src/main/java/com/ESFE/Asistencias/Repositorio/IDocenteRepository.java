c0abef0ca71a4f2e4511f45d9f6b84b5503b3f80
Antomarr14 <diegozetino8@gmail.com>
2024-08-12 16:55:55 -0600

    Funcionalidad completa para el modelo docente

---
+++ src/main/java/com/ESFE/Asistencias/Repositorio/IDocenteRepository.java
@@ -0,0 +1,9 @@
+package com.ESFE.Asistencias.Repositorio;
+
+import com.ESFE.Asistencias.Entidades.Docente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IDocenteRepository extends JpaRepository<Docente, Integer> {
+}
+
+
